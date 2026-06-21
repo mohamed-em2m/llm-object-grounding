3e80a18826086e28108bac29c226dcb3fbc8d15c
mohamed-em2m <2bl2ckm2n2@gmail.com>
2026-06-21 20:46:42 +0300

    chore: installing llama-server as a global executable

---
+++ scripts/install_llama_cpp.sh
@@ -0,0 +1,12 @@
+export CUDA_VISIBLE_DEVICES=0,1  # Use only the first two GPUs
+uv pip install  json-repair openai
+sudo apt-get update
+sudo apt-get install libnvidia-compute-535
+sudo apt-get install libnvidia-compute-535-server
+apt-get install pciutils build-essential cmake curl libcurl4-openssl-dev -y
+git clone https://github.com/ggml-org/llama.cpp
+cmake llama.cpp -B llama.cpp/build \
+    -DBUILD_SHARED_LIBS=OFF -DGGML_CUDA=ON
+cmake --build llama.cpp/build --config Release -j 24 --clean-first --target llama-cli llama-mtmd-cli llama-server llama-gguf-split
+cp llama.cpp/build/bin/llama-* llama.cpp
+install -m 755 llama.cpp/build/bin/llama-server /usr/local/bin/llama-server
